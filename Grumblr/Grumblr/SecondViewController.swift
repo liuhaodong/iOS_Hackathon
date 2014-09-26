e78e66c1bfdafba590c0b0e1cdd2302929dec4d1
Haodong Liu <haodongliu@haodongs-mbp.wv.cc.cmu.edu>
2014-09-26 10:46:46 -0400

    Add basic project

---
+++ Grumblr/Grumblr/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Grumblr
+//
+//  Created by Haodong Liu on 9/26/14.
+//  Copyright (c) 2014 Haodong Liu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
